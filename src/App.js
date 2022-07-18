4ef74dff175e1086c0bd494e40a396d2720ead66
Emily Santos <eyssantos@gmail.com>
2022-07-18 18:43:04 +0100

    add assets, home and nav route and nav bar

--- src/App.js
+++ src/App.js
@@ -1,37 +1,16 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './routes/home/home.component'
+import Navigation from './routes/navigation/navigation.component';
 
 const App = () => {
-  
-  const categories = [
-    {
-      "id": 1,
-      "title": "Hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      "id": 2,
-      "title": "Jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      "id": 3,
-      "title": "Trainers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      "id": 4,
-      "title": "Womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      "id": 5,
-      "title": "Mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-  
-  return <Directory categories={categories} />;
-
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />}/>
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
