610871f2b04a0a30ab2de05e41cfa264a127be5a
Emily Santos <eyssantos@gmail.com>
2022-07-22 16:29:26 +0100

    update the quantity and items into shopping bag

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { UserProvider } from "./contexts/user.context";
 import { ProductsProvider } from "./contexts/products.context";
+import { CartProvider } from "./contexts/cart.context";
 
 import './index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -16,14 +17,13 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>  
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,8 +1,12 @@
 import { Fragment, useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom'
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
@@ -10,7 +14,7 @@ import './navigation.styles.scss'
 
 const Navigation = () => {
   const { currentUser} = useContext(UserContext);
-
+  const { isCartOpen } = useContext(CartContext);
   return (
     <Fragment>
       <div className='navigation'>
@@ -26,8 +30,9 @@ const Navigation = () => {
           ) : (
             <Link className="nav-link" to='/auth'>SIGN IN</Link>
           )}
-          
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown/>}
       </div>
       <Outlet />
     </Fragment>
