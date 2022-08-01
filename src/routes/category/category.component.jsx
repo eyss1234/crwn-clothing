896adb999651e4120712f8675deecc223d6a13e4
Emily Santos <eyssantos@gmail.com>
2022-08-01 22:56:37 +0100

    fixed style clashes throughout app

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from 'react';
+import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
@@ -17,12 +17,15 @@ const Category = () => {
   }, [category, categoriesMap])
 
   return (
+    <Fragment>
+    <h2 className='category-title'>{category.toUpperCase()}</h2>
     <div className='category-container'>
       {products &&
         products.map((product) => 
           <ProductCard key={product.id} product={product} />)
       }
     </div>
+    </Fragment>
   )
 };
 
